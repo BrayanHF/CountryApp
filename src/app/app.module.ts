2dca4ecd3af0b8b06dffb397e057536c9eca24c2
BrayanHF <brayanhernandezfurnieles@gmail.com>
2023-08-10 14:19:06 -0500

    Practica de routing y vista de tabla de paises

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
+import { RoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent],
+  imports: [BrowserModule, SharedModule, RoutingModule, HttpClientModule],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { SearchBoxComponent } from './components/search-box/search-box.component';
+import { RouterModule } from '@angular/router';
+
+@NgModule({
+  declarations: [SidebarComponent, SearchBoxComponent],
+  exports: [SidebarComponent, SearchBoxComponent],
+  imports: [CommonModule, RouterModule],
+})
+export class SharedModule {}
